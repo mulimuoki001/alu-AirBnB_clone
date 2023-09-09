41fc96864a82ff7ddf3d31bd6cd89a3944ce16ff
mulimuoki001 <b.muoki@alustudent.com>
2023-09-09 09:03:03 +0200

    console update

--- console.py
+++ console.py
@@ -47,7 +47,7 @@ class HBNBCommand(cmd.Cmd):
         "Place", 
         "Amenity", 
         "Review"
-        }
+    }
 
     def emptyline(self):
         """Do nothing upon receiving an empty line."""
